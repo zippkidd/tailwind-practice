87f70ada7e730a0a84b7eb47953c1b1dce5e970c
zippkidd <thomastylek@gmail.com>
2020-02-04 21:41:34 -0500

    Installed postcss and set up config file, imported with @tailwind into layout.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-postcss`,
+    // {
+    //   resolve: "gatsby-plugin-purgecss",
+    //   options: {
+    //     tailwind: true,
+    //     purgeOnly: ["src/css/style.css"]
+    //   }
+    // }
   ],
 }
