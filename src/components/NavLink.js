5e0f009497739fbbd2013435dad60cf6b4e36613
zippkidd <thomastylek@gmail.com>
2020-02-07 00:02:15 -0500

    added some button styles

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 const NavLink = props => (
 	<Link 
 		to={props.to}
-		className="text-orange-400"
+		className="text-blue-600 border border-blue-600 px-5 py-2 font-bold text-lg rounded-full inline-block"
 	>
 		{props.children}
 	</Link>
